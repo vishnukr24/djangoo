15512f6424940f708eda4a11efe0d83ed24ddbdb
gokulsona <gokulsona19@gmail.com>
2024-08-06 09:50:51 +0530

    model department created

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class department(models.Model):
+    dept_name = models.CharField(max_length=255)
+    dept_description = models.TextField()
