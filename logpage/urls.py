af64b54fa98dd8968c374807e00f8311a829f99e
vishnukr24 <vishnukrmadakkathara@gmail.com>
2024-08-09 12:05:36 +0530

    hfjd

--- logpage/urls.py
+++ logpage/urls.py
@@ -1,24 +1,10 @@
-"""
-URL configuration for demoproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from . import views
-
+from .import views
 urlpatterns = [
-    path('', views.register,name='register'),
-    path('signin', views.signin,name='signin')
-]
+    path('home', views.register,name='home'),
+    path('login', views.signin,name='signin'),
+    path('',views.register,name='register'),
+    path('logout',views.logout,name='logout'),
+    path('viewall',views.viewall,name='viewall'),
+    path('upadatereg/<str:pk>/',views.updatereg,name='updatereg'),
+    ]
--- logpage/views.py
+++ logpage/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from . forms import RegisterForm
 from . models import Register
+from django.contrib import messages
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -20,7 +19,7 @@ def signin(request):
     if request.method == 'POST':
         name = request.POST['name']
         password = request.POST['pass']
-        user = Register.objects.get(name=name, password=password)
+        user = Register.objects.filter(name=name, password=password).first()
         if user is None:
             messages.error(request, 'invalid password')
             return redirect('signin')
@@ -28,3 +27,36 @@ def signin(request):
             request.session['name'] = name
             return redirect('home')
     return render(request, 'signin.html')
+def logout(request):
+    if 'name' in request.session:
+        request.session.flush()
+        return redirect('sigin')
+    return redirect('home')
+
+
+
+
+def viewall(request):
+    if 'uname' not in request.session:
+        return redirect('signin')
+    reg=Register.objects.all()
+    context={
+        'reg':reg,
+    }
+    return render(request,"viewreg.html",context)
+def updatereg(request,pk):
+    pass
+def updatedata(request,pk):
+    name=request.POST["name"]
+    phone=request.POST["phone"]
+    email=request.POST["email"]
+    username=request.POST["uname"]
+    password=request.POST["password"]
+    reg = Register.objects.get(id=pk)
+    reg.name=name
+    
+    
+
+
+
+
